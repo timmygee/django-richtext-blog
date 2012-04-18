e0b5b3e5f966d9576a1199885b184f415db484dc
Tim Godfrey <tim@wholebaked.com.au>
2012-04-18 23:59:04 +1000

    moved templates to template/appname as per correct convention http://stackoverflow.com/questions/4571686/django-include-template-from-another-app

--- richtext_blog/urls.py
+++ richtext_blog/urls.py
@@ -10,20 +10,21 @@ urlpatterns = patterns('',
     url(r'^$', RedirectToPostsView.as_view(permanent=True)),
     url(r'^posts/$', PostListView.as_view(
         paginate_by=10,
-        template_name='post-list.html',
+        template_name='richtext_blog/post-list.html',
         ), name='posts_all'),
     url(r'^(?P<year>[\d]{4})/$', PostListView.as_view(
         paginate_by=10,
-        template_name='post-list.html',
+        template_name='richtext_blog/post-list.html',
         ), name='posts_yearly'),
     url(r'^(?P<year>[\d]{4})/(?P<month>[\d]{2})/$', PostListView.as_view(
         paginate_by=10,
-        template_name='post-list.html',
+        template_name='richtext_blog/post-list.html',
         ), name='posts_monthly'),
     url(r'^(?P<year>[\d]{4})/(?P<month>[\d]{2})/(?P<slug>[-\w]+)/$',
-        PostView.as_view(template_name='post-detail.html'), name='post'),
+        PostView.as_view(template_name='richtext_blog/post-detail.html'),
+        name='post'),
     url(r'^tags/(?P<slug>[-\w]+)/$', TagView.as_view(
-        template_name='tag-view.html'), name='posts_tag'),
+        template_name='richtext_blog/tag-view.html'), name='posts_tag'),
     url(r'^rss/$', AllPostsRssFeed(), name='posts_all_rss'),
     url(r'^atom/$', AllPostsAtomFeed(), name='posts_all_atom')
     )
